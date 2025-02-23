45b8db250023a7776df8d439310e55f4098bd256
immprada <im.mprada@gmail.com>
2025-02-22 19:18:18 -0500

    [any message]

--- app/server.rb
+++ app/server.rb
@@ -8,11 +8,11 @@ print("Logs from your program will appear here!")
 
 # Uncomment this block to pass the first stage
 #
-# loop do
-#   data, (_, sender_port, _, sender_ip) = udp_socket.recvfrom(512)
-#   response = ""
-#   udp_socket.send(response, 0, sender_ip, sender_port)
-# rescue => e
-#   puts "Error receiving data: #{e}"
-#   break
-# end
+loop do
+  data, (_, sender_port, _, sender_ip) = udp_socket.recvfrom(512)
+  response = ""
+  udp_socket.send(response, 0, sender_ip, sender_port)
+rescue => e
+  puts "Error receiving data: #{e}"
+  break
+end
